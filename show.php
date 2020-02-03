43ed531221358ee794ef2243f2bfaf846d76265f
Ben Border <ben@bunnbuns.net>
2020-02-03 09:27:55 -0500

    new stuffs

--- show.php
+++ show.php
@@ -8,10 +8,10 @@ if(!$username or $password == ""){
     header('Location: ../bookmark/?error=no-info');
 }else{
 
-$js = 'document.body.innerHTML = \'<h1>Loading...</h1><form style="display:none" id="loginform" accept-charset="utf-8" method="POST" action="https://ls.zebras.net:442/login" name="LOGINFORM"> <input type="text" name="USERNAME" value="'.$username.'" size="15" maxlength="255"><input type="password" name="PASSWORD" size="15" value="'.$password.'"> <input type="submit" name="ACTION"> </form>\'; document.getElementById("loginform").submit();';
+$js = 'document.body.innerHTML = \'<h1 style="color:#00BBB4;"><form style="display:none" id="loginform" accept-charset="utf-8" method="POST" action="https://ls.zebras.net:442/login" name="LOGINFORM"> <input type="text" name="USERNAME" value="'.$username.'" size="15" maxlength="255"><input type="password" name="PASSWORD" size="15" value="'.$password.'"> <input type="submit" name="ACTION"> </form>\'; document.getElementById("loginform").submit();';
 
 $jsOut = 'console.log("Sending login data");eval(atob("'.base64_encode($js).'"))';
-$htmlOut = "<body></body><script>".$jsOut."</script>";
+$htmlOut = '<body style="font-size:12px;font-family:Arial, Helvetica, sans-serif;text-align:center;"></body><script>'.$jsOut.'</script>';
 $base64out = base64_encode($htmlOut);
 
 $url = "data:text/html;base64,".$base64out;
